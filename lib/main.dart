a5b6e2a84a565676b4ea567528a5668e9ed6944f
Ernest Ndungu <ernjohn@microsoft.com>
2020-03-03 22:36:28 +0200

    azure backend added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_rest_api/rest_api.dart';
 
@@ -8,29 +10,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.blue,
       ),
-      home: EmployeePage(),
+      home: EventPage(),
     );
   }
 }
 
-class EmployeePage extends StatefulWidget {
+class EventPage extends StatefulWidget {
   @override
-  _EmployeePageState createState() => _EmployeePageState();
+  _EventPageState createState() => _EventPageState();
 }
 
-class _EmployeePageState extends State<EmployeePage> {
+class _EventPageState extends State<EventPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter REST API'),
+        title: Text('Azure for Android'),
       ),
       body: FutureBuilder(
-        future: ApiService.getEmployees(),
+        future: ApiService.getEvents(),
         builder: (context, snapshot) {
-          final employees = snapshot.data;
+          final events = snapshot.data;
           if (snapshot.connectionState == ConnectionState.done) {
             return ListView.separated(
               separatorBuilder: (context, index) {
@@ -40,12 +42,16 @@ class _EmployeePageState extends State<EmployeePage> {
                 );
               },
               itemBuilder: (context, index) {
+                
+                //This is where the Event list is populated
                 return ListTile(
-                  title: Text(employees[index]['employee_name']),
-                  subtitle: Text('Age: ${employees[index]['employee_age']}'),
+                  title: Text(events[index]['name']),
+                  subtitle: Text('Venue: ${events[index]['venue']}'),
+                  
                 );
+
               },
-              itemCount: employees.length,
+              itemCount: events.length,
             );
           }
           return Center(
@@ -58,31 +64,60 @@ class _EmployeePageState extends State<EmployeePage> {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => AddNewEmployeePage(),
+              builder: (context) => AddNewEventPage(),
             ),
           );
         },
-        tooltip: 'Increment',
+        tooltip: 'New Event',
         child: Icon(Icons.add),
       ),
     );
   }
 }
 
-class AddNewEmployeePage extends StatefulWidget {
-  AddNewEmployeePage({Key key}) : super(key: key);
+class Uuid {
+  final Random _random = Random();
 
-  _AddNewEmployeePageState createState() => _AddNewEmployeePageState();
+  /// Generate a version 4 (random) uuid. This is a uuid scheme that only uses
+  /// random numbers as the source of the generated uuid.
+  String generateV4() {
+    // Generate xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx / 8-4-4-4-12.
+    final int special = 8 + _random.nextInt(4);
+
+    return '${_bitsDigits(16, 4)}${_bitsDigits(16, 4)}-'
+        '${_bitsDigits(16, 4)}-'
+        '4${_bitsDigits(12, 3)}-'
+        '${_printDigits(special, 1)}${_bitsDigits(12, 3)}-'
+        '${_bitsDigits(16, 4)}${_bitsDigits(16, 4)}${_bitsDigits(16, 4)}';
+  }
+
+  String _bitsDigits(int bitCount, int digitCount) =>
+      _printDigits(_generateBits(bitCount), digitCount);
+
+  int _generateBits(int bitCount) => _random.nextInt(1 << bitCount);
+
+  String _printDigits(int value, int count) =>
+      value.toRadixString(16).padLeft(count, '0');
 }
 
-class _AddNewEmployeePageState extends State<AddNewEmployeePage> {
-  final _employeeNameController = TextEditingController();
-  final _employeeAge = TextEditingController();
+class AddNewEventPage extends StatefulWidget {
+  AddNewEventPage({Key key}) : super(key: key);
+
+  _AddNewEventPageState createState() => _AddNewEventPageState();
+}
+
+class _AddNewEventPageState extends State<AddNewEventPage> {
+  final _eventNameController = TextEditingController();
+  final _eventDateController = TextEditingController();
+  final _eventAttendeesController = TextEditingController();
+  final _eventVenueController = TextEditingController();
+    
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('New Employee'),
+        title: Text('New Event'),
       ),
       body: Center(
         child: Padding(
@@ -90,13 +125,23 @@ class _AddNewEmployeePageState extends State<AddNewEmployeePage> {
           child: Column(
             children: <Widget>[
               TextField(
-                controller: _employeeNameController,
-                decoration: InputDecoration(hintText: 'Employee Name'),
+                controller: _eventNameController,
+                decoration: InputDecoration(hintText: 'Event Name'),
               ),
               TextField(
-                controller: _employeeAge,
-                decoration: InputDecoration(hintText: 'Employee Age'),
+                controller: _eventDateController,
+                decoration: InputDecoration(hintText: 'Event Date'),
+                keyboardType: TextInputType.datetime,
+              ),
+              TextField(
+                controller: _eventAttendeesController,
+                decoration: InputDecoration(hintText: 'Number of Attendees'),
                 keyboardType: TextInputType.number,
+              ),
+              TextField(
+                controller: _eventVenueController,
+                decoration: InputDecoration(hintText: 'Venue'),
+                keyboardType: TextInputType.datetime,
               ),
               RaisedButton(
                 child: Text(
@@ -106,22 +151,31 @@ class _AddNewEmployeePageState extends State<AddNewEmployeePage> {
                   ),
                 ),
                 color: Colors.purple,
+                //This is where the Json is Built
                 onPressed: () {
                   final body = {
-                    "name": _employeeNameController.text,
-                    "age": _employeeAge.text,
+                    "id": Uuid().generateV4(),
+                    "name": _eventNameController.text,
+                    "date": _eventDateController.text,
+                    "attendees": _eventAttendeesController.text,
+                    "venue": _eventVenueController.text,
                   };
-                  ApiService.addEmployee(body).then((success) {
+
+
+                  ApiService.addEvent(body).then((success) {
                     if (success) {
                       showDialog(
                         builder: (context) => AlertDialog(
-                          title: Text('Employee has been added!!!'),
+                          title: Text('Congratulations, Event has been added!'),
                           actions: <Widget>[
                             FlatButton(
                               onPressed: () {
                                 Navigator.pop(context);
-                                _employeeNameController.text = '';
-                                _employeeAge.text = '';
+                               
+                                _eventNameController.text = '';
+                                _eventDateController.text = '';
+                                _eventAttendeesController.text = '';
+                                _eventVenueController.text = '';
                               },
                               child: Text('OK'),
                             )
@@ -133,7 +187,7 @@ class _AddNewEmployeePageState extends State<AddNewEmployeePage> {
                     }else{
                       showDialog(
                         builder: (context) => AlertDialog(
-                          title: Text('Error Adding Employee!!!'),
+                          title: Text('Error Adding Event!!!'),
                           actions: <Widget>[
                             FlatButton(
                               onPressed: () {
--- lib/rest_api.dart
+++ lib/rest_api.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
 class URLS {
-  static const String BASE_URL = 'http://dummy.restapiexample.com/api/v1';
+  static const String POST_URL = 'https://prod-63.westeurope.logic.azure.com:443/workflows/045043bcb0604d8a8fa4cdafadd26bae/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=8Xgx5KYmVSUpnjXyrU5B_01lZ_63H-xQbfh_cmPIjfQ';
+  static const String GET_URL = 'https://prod-45.westeurope.logic.azure.com:443/workflows/fb85053502c7447292c73a6409ff18eb/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1TBzE0hNP0MmOp1MAP6cYKBjl1zToY8qo0qfQhPRS0g';
 }
 
 class ApiService {
-  static Future<List<dynamic>> getEmployees() async {
+  static Future<List<dynamic>> getEvents() async {
     // RESPONSE JSON :
     // [{
     //   "id": "1",
@@ -16,7 +18,7 @@ class ApiService {
     //   "employee_age": "0",
     //   "profile_image": ""
     // }]
-    final response = await http.get('${URLS.BASE_URL}/employees');
+    final response = await http.get(URLS.GET_URL);
     if (response.statusCode == 200) {
       return json.decode(response.body);
     } else {
@@ -24,13 +26,26 @@ class ApiService {
     }
   }
 
-  static Future<bool> addEmployee(body) async {
+  static Future<bool> addEvent(body) async {
     // BODY
     // {
     //   "name": "test",
     //   "age": "23"
     // }
-    final response = await http.post('${URLS.BASE_URL}/create', body: body);
+   // final response = await http.post(
+   //   URLS.POST_URL,
+    //  headers:{HttpHeaders.contentTypeHeader : ContentType.json},
+    //   body: body);
+    
+    Map<String,String> headers = {
+      'Content-type' : 'application/json', 
+      'Accept': 'application/json',
+    };
+
+    final response =
+        await http.post(URLS.POST_URL, body: json.encode(body), headers: headers);
+    
+
     if (response.statusCode == 200) {
       return true;
     } else {
